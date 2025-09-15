c5d87fa50c47766f88317c984f639bfcd2f00a05
Joe Harkins <Harkoe92@hotmail.com>
2025-09-15 16:11:31 -0700

    Created File Struct
    Started over, remade file structure and began again

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Homescreeen';
+
+function App() {
+  return (
+    <div className="App">  
+    <Home />
+    </div>
+   
+  );
+}
+
+export default App;
