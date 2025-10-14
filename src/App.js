34f4637e1f818ec3f75563fb108d5273368fe824
Joe Harkins <Harkoe92@hotmail.com>
2025-10-14 00:59:24 -0700

    Posted
    Launched website to github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './pages/Home/Navbar';
 import Home from './pages/Home/Homescreeen';
 import Carousel from './pages/Home/AboutMe'
