3e43f748c3ffbdd67537fb7e73b910fdf322137e
Joe Harkins <Harkoe92@hotmail.com>
2025-10-15 10:41:39 -0700

    Update
    Updated Resume link and Github links

--- src/pages/Home/ResumeButton.jsx
+++ src/pages/Home/ResumeButton.jsx
@@ -1,7 +1,7 @@
 function ResumeButton() {
   return (
     <a 
-      href="/Joe Harkins Resume 2025 (1).pdf" 
+      href="/Resume.pdf" 
       target="_blank" 
       rel="noopener noreferrer" 
       className="btn btn-primary" // Add your CSS class here
